763d7aad2c850ddadbae5cd0646e6d50d748e08d
ap_boot <sahapri@gmail.com>
2023-07-09 17:45:16 -0400

    for Learning Assistant

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,22 @@
+const cards = document.getElementsByClassName('card');
+console.log(cards);
+
 const logout = async () => {
-  const response = await fetch("/api/users/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace('/');
   } else {
-    alert("Failed to log out.");
+    alert('Failed to log out.');
   }
 };
 
-document.querySelector("#logout").addEventListener("click", logout);
+cards.forEach((item) => {
+  item.addEventListener('click', function (event) {
+    console.log(event);
+  });
+});
+document.querySelector('#logout').addEventListener('click', logout);
